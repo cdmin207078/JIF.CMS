5fbe4d6f9a86785d53856f147164361c85ad04c5
cdmin207078 <cdmin207078@foxmail.com>
2017-04-18 19:03:22 +0800

    web upload foo

---
+++ code/JIF.CMS/JIF.CMS.Management/js/attachment.js
@@ -0,0 +1,244 @@
+﻿var attachmentUpload = (function ($) {
+
+    var $wrap = $('#uploader'),
+
+        // 上传按钮
+        $upload = $wrap.find('.upload-btn'),
+
+        // 添加的文件数量
+        fileCount = 0,
+
+        // 添加的文件总大小
+        fileSize = 0,
+
+        // 可能有pedding, ready, uploading, confirm, done.
+        state = 'pedding',
+
+        // 所有文件的进度信息，key为file id
+        percentages = {},
+
+        // 判断浏览器是否支持图片的base64
+        isSupportBase64 = (function () {
+            var data = new Image();
+            var support = true;
+            data.onload = data.onerror = function () {
+                if (this.width != 1 || this.height != 1) {
+                    support = false;
+                }
+            }
+            data.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+            return support;
+        })(),
+
+        // 检测是否已经安装flash，检测flash的版本
+        flashVersion = (function () {
+            var version;
+
+            try {
+                version = navigator.plugins['Shockwave Flash'];
+                version = version.description;
+            } catch (ex) {
+                try {
+                    version = new ActiveXObject('ShockwaveFlash.ShockwaveFlash')
+                      .GetVariable('$version');
+                } catch (ex2) {
+                    version = '0.0';
+                }
+            }
+            version = version.match(/\d+/g);
+            return parseFloat(version[0] + '.' + version[1], 10);
+        })(),
+
+        // WebUploader实例
+        uploader;
+
+
+    if (!WebUploader.Uploader.support('flash') && WebUploader.browser.ie) {
+
+        // flash 安装了但是版本过低。
+        if (flashVersion) {
+            (function (container) {
+                window['expressinstallcallback'] = function (state) {
+                    switch (state) {
+                        case 'Download.Cancelled':
+                            alert('您取消了更新！')
+                            break;
+
+                        case 'Download.Failed':
+                            alert('安装失败')
+                            break;
+
+                        default:
+                            alert('安装已成功，请刷新！');
+                            break;
+                    }
+                    delete window['expressinstallcallback'];
+                };
+
+                var swf = './expressInstall.swf';
+                // insert flash object
+                var html = '<object type="application/' +
+                        'x-shockwave-flash" data="' + swf + '" ';
+
+                if (WebUploader.browser.ie) {
+                    html += 'classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" ';
+                }
+
+                html += 'width="100%" height="100%" style="outline:0">' +
+                    '<param name="movie" value="' + swf + '" />' +
+                    '<param name="wmode" value="transparent" />' +
+                    '<param name="allowscriptaccess" value="always" />' +
+                '</object>';
+
+                container.html(html);
+
+            })($wrap);
+
+            // 压根就没有安转。
+        } else {
+            $wrap.html('<a href="http://www.adobe.com/go/getflashplayer" target="_blank" border="0"><img alt="get flash player" src="http://www.adobe.com/macromedia/style_guide/images/160x41_Get_Flash_Player.jpg" /></a>');
+        }
+
+        return;
+    } else if (!WebUploader.Uploader.support()) {
+        alert('Web Uploader 不支持您的浏览器！');
+        return;
+    }
+
+    var init = function () {
+
+        initWebUploader();
+        initElemEvents();
+    }
+
+
+    var setState = function (val) {
+        var file, stats;
+
+        if (val === state) {
+            return;
+        }
+
+        $upload.removeClass('state-' + state);
+        $upload.addClass('state-' + val);
+        state = val;
+
+        switch (state) {
+            case 'pedding':
+                $placeHolder.removeClass('element-invisible');
+                $queue.hide();
+                $statusBar.addClass('element-invisible');
+                uploader.refresh();
+                break;
+
+            case 'ready':
+                $placeHolder.addClass('element-invisible');
+                $('#filePicker2').removeClass('element-invisible');
+                $queue.show();
+                $statusBar.removeClass('element-invisible');
+                uploader.refresh();
+                break;
+
+            case 'uploading':
+                $('#filePicker2').addClass('element-invisible');
+                $progress.show();
+                $upload.text('暂停上传');
+                break;
+
+            case 'paused':
+                $progress.show();
+                $upload.text('继续上传');
+                break;
+
+            case 'confirm':
+                $progress.hide();
+                $('#filePicker2').removeClass('element-invisible');
+                $upload.text('开始上传');
+
+                stats = uploader.getStats();
+                if (stats.successNum && !stats.uploadFailNum) {
+                    setState('finish');
+                    return;
+                }
+                break;
+            case 'finish':
+                stats = uploader.getStats();
+                if (stats.successNum) {
+                    alert('上传成功');
+                } else {
+                    // 没有成功的图片，重设
+                    state = 'done';
+                    location.reload();
+                }
+                break;
+        }
+
+        updateStatus();
+    }
+
+
+    var initWebUploader = function () {
+
+        // 实例化
+        uploader = WebUploader.create({
+            // 选择文件的按钮。可选。
+            // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+            pick: '#picker',
+
+            // swf文件路径
+            swf: '~/Content/webuploader-0.1.5/Uploader.swf',
+
+            // 文件接收服务端。
+            server: '/Attachment/Upload',
+
+
+            // 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
+            resize: false
+        });
+
+
+
+        uploader.onFileQueued = function (file) {
+            fileCount++;
+            fileSize += file.size;
+
+            setState('ready');
+        }
+        // 当有文件被添加进队列的时候
+        uploader.on('fileQueued', function (file) {
+            $('#thelist').append('<div id="' + file.id + '" class="item">' +
+                '<h4 class="info">' + file.name + '</h4>' +
+                '<p class="state">等待上传...</p>' +
+            '</div>');
+        });
+
+        uploader.on('ready', function () {
+            window.uploader = uploader;
+        });
+
+    }
+
+    var initElemEvents = function () {
+
+        $upload.on('click', function () {
+            if ($(this).hasClass('disabled')) {
+                return false;
+            }
+
+            console.log('$upload onclick ' + state)
+
+            if (state === 'ready') {
+                uploader.upload();
+            } else if (state === 'paused') {
+                uploader.upload();
+            } else if (state === 'uploading') {
+                uploader.stop();
+            }
+        });
+    }
+
+    return {
+        init: init
+    };
+
+})(jQuery);
