7f40632829a5e2ba1ecfb965d6e66315a662727b
cdmin207078 <cdmin207078@foxmail.com>
2017-10-31 14:14:25 +0800

    add EnumExtensions update references versions

---
+++ code/JIF.CMS/JIF.CMS.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JIF.CMS.Core.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// 获取枚举值说明文字
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum value)
+        {
+            string str = value.ToString();
+            System.Reflection.FieldInfo field = value.GetType().GetField(str);
+            object[] objs = field.GetCustomAttributes(typeof(System.ComponentModel.DescriptionAttribute), false);
+            if (objs == null || objs.Length == 0) return str;
+            System.ComponentModel.DescriptionAttribute da = (System.ComponentModel.DescriptionAttribute)objs[0];
+            return da.Description;
+        }
+    }
+}
---
+++ code/JIF.CMS/JIF.CMS.Test.Core/Tests/EnumExtensionsTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using JIF.CMS.Core.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JIF.CMS.Test.Core.Tests
+{
+    [TestClass]
+    public class EnumExtensionsTest
+    {
+        [TestMethod]
+        public void Enum_Class_Method_Test()
+        {
+            // 获取枚举所有值
+            var names = Enum.GetNames(typeof(EnumExtensionsType));
+            var values = Enum.GetValues(typeof(EnumExtensionsType));
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("{0} = {1}", names[i], values.GetValue(i));
+            }
+
+            // 根据数值, 获取指定枚举名称
+            Console.WriteLine(Enum.GetName(typeof(EnumExtensionsType), 5)); // 不存在枚举值, 返回空字符串
+            Console.WriteLine(Enum.GetName(typeof(EnumExtensionsType), 3));
+
+        }
+
+        [TestMethod]
+        public void GetDescription_Test()
+        {
+            Console.WriteLine(EnumExtensionsType.Android);
+            Console.WriteLine(EnumExtensionsType.Android.ToString());
+            Console.WriteLine(EnumExtensionsType.Android.GetDescription());
+
+            Console.WriteLine(EnumExtensionsType.BlackBerry);
+            Console.WriteLine(EnumExtensionsType.BlackBerry.GetDescription());
+        }
+
+
+        enum EnumExtensionsType
+        {
+            [System.ComponentModel.Description("谷歌")]
+            Android,
+
+            [System.ComponentModel.Description("苹果")]
+            IOS,
+
+            [System.ComponentModel.Description("微软")]
+            WP,
+
+            BlackBerry
+        }
+    }
+}
