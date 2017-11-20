4a01a68b7d4da4e1d26d4a56b2d987e30b96e75f
cdmin207078 <cdmin207078@foxmail.com>
2017-11-20 19:42:53 +0800

    foo

---
+++ code/JIF.CMS/JIF.CMS.Test.Core/Tests/CommonTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JIF.CMS.Test.Core.Tests
+{
+    [TestClass]
+    public class CommonTest
+    {
+        [TestMethod]
+        public void Exception_Performance_Test()
+        {
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    var a = 1;
+                    var b = 0;
+                    var c = a / b;
+                }
+                catch { }
+            }
+
+            watch.Show("抛出异常");
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    try
+                    {
+                        var a = 1;
+                        var b = 0;
+                        var c = a / b;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+                catch
+                {
+                }
+
+            }
+
+            watch.Show("抛出嵌套异常");
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    try
+                    {
+                        try
+                        {
+                            var a = 1;
+                            var b = 0;
+                            var c = a / b;
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+                catch { }
+            }
+
+            watch.Show("三个套异常");
+
+
+            for (int i = 0; i < 100000; i++)
+            {
+                var a = 1;
+                var b = 1;
+                var c = a / b;
+            }
+            watch.Show("无异常");
+        }
+    }
+}
