7439f5f61160c0e6e6209767e41aaf9be63fc0b1
且歌且码 <cdmin207078@foxmail.com>
2017-11-23 01:26:53 +0800

    update PerformanceTest

--- code/JIF.CMS/JIF.CMS.Test.Repositories/PerformanceTest.cs
+++ code/JIF.CMS/JIF.CMS.Test.Repositories/PerformanceTest.cs
@@ -27,14 +27,14 @@ namespace JIF.CMS.Test.Repositories
         /// database connection string
         /// </summary>
         //private readonly string connectionstring = "Data Source=192.168.0.130;Initial Catalog=RepositoriesPerformanceTestDB;User ID=sa;Password=eva349121171@;";
-        private readonly string connectionstring = "Data Source=43.247.90.5;Initial Catalog=RepositoriesPerformanceTestDB;User ID=sa;Password=d4ihXjQLg3w6xn2IB5XM;";
+        private readonly string connectionstring = "Data Source=43.247.90.5;Initial Catalog=RepositoriesPerformanceTestDB;User ID=sa;Password=d4ihXjQLg3w6xn2IB5XM;MultipleActiveResultSets=true";
 
-        private List<UserInfo> GenSomeUsers()
+        private List<UserInfo> GenSomeUsers(int count)
         {
             var users = new List<UserInfo>();
-            var names = RandomHelper.GenChinesePersonName(10000);
+            var names = RandomHelper.GenChinesePersonName(count);
 
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < count; i++)
             {
                 var u = new UserInfo
                 {
@@ -73,92 +73,82 @@ namespace JIF.CMS.Test.Repositories
         }
 
         [TestMethod]
-        public void EF_Insert_Test()
+        public void Insert_Performance_Test()
         {
-            var db = getEfDbContext();
-
+            var ef = getEfDbContext();
 
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            var users = GenSomeUsers();
+            var users = GenSomeUsers(10000);
             watch.Show("生成用户");
 
-            db.Set<UserInfo>().AddRange(users);
-            db.SaveChanges();
-            watch.Show("插入用户");
+            ef.Set<UserInfo>().AddRange(users);
+            ef.SaveChanges();
+            watch.Show("ef 插入用户");
+
+            IDbConnection dap = new SqlConnection(connectionstring);
+
+            dap.Insert(users);
+            watch.Show("dapper 插入用户");
         }
 
         [TestMethod]
-        public void EF_Query_Test()
+        public void Query_Performance_Test()
         {
-            var db = getEfDbContext();
+            var ef = getEfDbContext();
 
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            var users1 = db.Set<UserInfo>().ToList();
+            var u1 = ef.Set<UserInfo>().ToList();
+            watch.Show("ef 查询所有不加条件");
+
+            var u2 = ef.Set<UserInfo>().Where(d => d.CreateTime > DateTime.Now).ToList();
+            watch.Show("ef 以时间为单一条件");
+
+            var u3 = ef.Set<UserInfo>().Where(d => d.Id > 100).ToList();
+            watch.Show("ef 以Id为条件");
+
+
+
+            IDbConnection dap = new SqlConnection(connectionstring);
+
+            var u4 = dap.Query<UserInfo>("select * from user_info").ToList();
             watch.Show("查询所有不加条件");
 
-            var users2 = db.Set<UserInfo>().Where(d => d.CreateTime > DateTime.Now).ToList();
+            var u5 = dap.Query<UserInfo>("select * from user_info where createTime > getdate()").ToList();
             watch.Show("以时间为单一条件");
 
-            var users3 = db.Set<UserInfo>().Where(d => d.Id > 100).ToList();
+            var u6 = dap.Query<UserInfo>("select * from user_info where id > 100").ToList();
             watch.Show("以Id为条件");
+
         }
 
         [TestMethod]
-        public void Dapper_Insert_Test()
-        {
-            IDbConnection db = new SqlConnection(connectionstring);
-
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-
-            var users = GenSomeUsers();
-            watch.Show("生成用户");
-
-            db.Insert(users);
-            watch.Show("插入用户");
-        }
-
-        [TestMethod]
-        public void Dapper_Query_Test()
-        {
-            IDbConnection db = new SqlConnection(connectionstring);
-
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-
-            var users1 = db.Query<UserInfo>("select * from user_info").ToList();
-            watch.Show("查询所有不加条件");
-
-
-            var users2 = db.Query<UserInfo>("select * from user_info where createTime > getdate()").ToList();
-            watch.Show("以时间为单一条件");
-
-            var users3 = db.Query<UserInfo>("select * from user_info where id > 100").ToList();
-            watch.Show("以Id为条件");
-        }
-
-
-
-        [TestMethod]
-        public void IDbConnection_New_Test()
+        public void IDbConnection_Using_New_Test()
         {
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 100; i++)
             {
                 IDbConnection db = new SqlConnection(connectionstring);
                 db.ExecuteScalar<int>("select count(1) from user_info;");
             }
-
             watch.Show("每次new 新的connection");
 
+            for (int i = 0; i < 100; i++)
+            {
+                using (IDbConnection db = new SqlConnection(connectionstring))
+                {
+                    db.ExecuteScalar<int>("select count(1) from user_info;");
+                }
+            }
+            watch.Show("使用using");
+
             IDbConnection shareConnection = new SqlConnection(connectionstring);
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 100; i++)
             {
                 shareConnection.ExecuteScalar<int>("select count(1) from user_info;");
             }
@@ -166,13 +156,70 @@ namespace JIF.CMS.Test.Repositories
         }
 
         [TestMethod]
-        public void IDbConnection_Async_Multi_Test()
+        public async Task IDbConnection_Async_Multi_Test()
         {
+            var logger = LogManager.GetLogger("");
+            IDbConnection shareConnection = new SqlConnection(connectionstring);
+
+            var tasks = new List<Task>();
+
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            var logger = LogManager.GetLogger("");
-            logger.Info("IDbConnection_Async_Multi_Test");
+            for (int i = 0; i < 100; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    // wrong: System.InvalidOperationException: 连接未关闭。 连接的当前状态为正在连接。
+                    //var count = shareConnection.ExecuteScalar<int>("select count(1) from user_info;");
+                    //logger.Info(string.Format("count: {0}, thread: {1}", count, Thread.CurrentThread.ManagedThreadId));
+
+                    using (IDbConnection unitConnection = new SqlConnection(connectionstring))
+                    {
+                        var count = unitConnection.ExecuteScalar<int>("select count(1) from user_info;");
+                        logger.Info(string.Format("count: {0}, thread: {1}", count, Thread.CurrentThread.ManagedThreadId));
+                    }
+                }));
+            }
+            await Task.WhenAll(tasks);
+            watch.Show("100 个线程同时访问");
+
+            tasks.Clear();
+
+            // wrong: System.InvalidOperationException: 无效操作。连接被关闭.
+            // note: 在使用中 不可使用同一dbconnection 来执行异步任务等待.
+            for (int i = 0; i < 100; i++)
+            {
+                tasks.Add(shareConnection.ExecuteScalarAsync<int>("select count(1) from user_info;"));
+            }
+            await Task.WhenAll(tasks);
+            watch.Show("100 个异步方法 - ExecuteScalarAsync 执行");
+
+            for (int i = 0; i < 100; i++)
+            {
+                var count = shareConnection.ExecuteScalar<int>("select count(1) from user_info;");
+                logger.Info(string.Format("count: {0}, thread: {1}", count, Thread.CurrentThread.ManagedThreadId));
+            }
+
+            watch.Show("同步执行100次");
+
+        }
+
+        [TestMethod]
+        public async Task Dapper_Async_Methods_Test()
+        {
+            IDbConnection shareConnection = new SqlConnection(connectionstring);
+
+            var c1 = shareConnection.ExecuteScalarAsync<int>("select count(1) from user_info;");
+            var c2 = shareConnection.ExecuteScalarAsync<int>("select count(1) from user_info;");
+            var c3 = shareConnection.ExecuteScalarAsync<int>("select count(1) from user_info;");
+            var c4 = shareConnection.ExecuteScalarAsync<int>("select count(1) from user_info;");
+
+            await (c1);
+            await (c2);
+            await (c3);
+            await (c4);
         }
     }
 }
+
