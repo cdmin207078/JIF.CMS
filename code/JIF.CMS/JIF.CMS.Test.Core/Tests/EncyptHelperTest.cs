2853e0018a6f7d8a86d43b28943006abcb231fe5
cdmin207078 <cdmin207078@foxmail.com>
2017-11-29 18:11:18 +0800

    foo

--- code/JIF.CMS/JIF.CMS.Test.Core/Tests/EncyptHelperTest.cs
+++ code/JIF.CMS/JIF.CMS.Test.Core/Tests/EncyptHelperTest.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using JIF.CMS.Core.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Web.Security;
 
 namespace JIF.CMS.Test.Core.Tests
 {
@@ -8,11 +7,19 @@ namespace JIF.CMS.Test.Core.Tests
     public class EncyptHelperTest
     {
         [TestMethod]
-        public void MyTestMethod()
+        public void Test_MD5()
         {
-            var ss = "B8C2EADF682D5BD4960BCB511CF65F8CECDDAD31FF572B81AEA186F1E02E562A4A048DD9C39BFE7AA7A59E4FFF42C564F44FF5B4B1169FD59E375529657ADC947F3BD1F21D484A96D120E4B30AB8707FFDF0D3779AE9FF8CB8EA2F5929363CA1FEBFF4839B1547669345D10CDD7383FC";
+            // 获得加密算法
+            var algo = EncyptHelper.CreateHashAlgoMd5();
+            var origin = "123456";
+            var cipher = EncyptHelper.Encrypt(algo, origin);
 
-            var ticket = FormsAuthentication.Decrypt(ss);
+            System.Console.WriteLine(cipher);
+        }
+
+        [TestMethod]
+        public void Test_RSA()
+        {
 
         }
     }
